e1cb69978375a252cfa85c56795cae27fecf1966
PavelZakharov <pavelzakharov1987@gmail.com>
2020-04-06 10:52:45 +0300

    feat: add tab, ctrl, shift, win, enter, space from mouse

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ textArea.focus();
 
 document.querySelector('.wrapper').append(divKeyBoard);
 
-
 const keyBoard = {
   rowFirst: {
     Backquote: ['`', '~', 'ё', 'Ё'], Digit1: ['1', '!', '1', '!'], Digit2: ['2', '@', '2', '"'], Digit3: ['3', '#', '3', '№'], Digit4: ['4', '$', '4', ';'], Digit5: ['5', '%', '5', '%'], Digit6: ['6', '^', '6', ':'], Digit7: ['7', '&', '7', '?'], Digit8: ['8', '*', '8', '*'], Digit9: ['9', '(', '9', '('], Digit0: ['0', ')', '0', ')'], Minus: ['-', '_', '-', '_'], Equal: ['=', '+', '=', '+'], Backspace: ['Backspace', 'Backspace', 'Backspace', 'Backspace'],
@@ -24,67 +23,133 @@ const keyBoard = {
     ShiftLeft: ['Shift', 'Shift', 'Shift', 'Shift'], KeyZ: ['z', 'Z', 'я', 'Я'], KeyX: ['x', 'X', 'ч', 'Ч'], KeyC: ['c', 'C', 'с', 'С'], KeyV: ['v', 'V', 'м', 'М'], KeyB: ['b', 'B', 'и', 'И'], KeyN: ['n', 'N', 'т', 'Т'], KeyM: ['m', 'M', 'ь', 'Ь'], Comma: [',', '<', 'б', 'Б'], Period: ['.', '>', 'ю', 'Ю'], Slash: ['/', '?', '.', ','], ArrowUp: ['AU', 'AU', 'AU', 'AU'], ShiftRight: ['Shift', 'Shift', 'Shift', 'Shift'],
   },
   rowFifth: {
-    ControlLeft: ['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'], MetaLeft: ['Win', 'Win', 'Win', 'Win'], AltLeft: ['Alt', 'Alt', 'Alt', 'Alt'], Space: ['', '', '', ''], AltRight: ['Alt', 'Alt', 'Alt', 'Alt'], ControlRight: ['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'], ArrowLeft: ['AL', 'AL', 'AL', 'AL'], ArrowDown: ['AD', 'AD', 'AD', 'AD'], ArrowRight: ['AR', 'AR', 'AR', 'AR'],
+    ControlLeft: ['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'], MetaLeft: ['Win', 'Win', 'Win', 'Win'], AltLeft: ['Alt', 'Alt', 'Alt', 'Alt'], Space: ['Space', 'Space', 'Space', 'Space'], AltRight: ['Alt', 'Alt', 'Alt', 'Alt'], ControlRight: ['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'], ArrowLeft: ['AL', 'AL', 'AL', 'AL'], ArrowDown: ['AD', 'AD', 'AD', 'AD'], ArrowRight: ['AR', 'AR', 'AR', 'AR'],
   },
 };
 
 for (const row in keyBoard) {
   const divRow = document.createElement('div');
-  divRow.className = 'row ' + `${row}`;
+  divRow.className = `row ${row}`;
   document.querySelector('.keyboard').append(divRow);
   for (const keys in keyBoard[row]) {
     const divKey = document.createElement('div');
     const spanEng = document.createElement('span');
     const spanRus = document.createElement('span');
-    divKey.className = 'key ' + `${keys}`;
+    divKey.className = `key ${keys}`;
     spanEng.className = 'eng';
     spanRus.className = 'rus hidden';
-    document.querySelector('.' + `${row}`).append(divKey);
-    document.querySelector('.' + `${keys}`).append(spanEng);
-    document.querySelector('.' + `${keys}`).append(spanRus);
+    document.querySelector(`.${row}`).append(divKey);
+    document.querySelector(`.${keys}`).append(spanEng);
+    document.querySelector(`.${keys}`).append(spanRus);
     for (let i = 0; i < keyBoard[row][keys].length; i += 1) {
       const span = document.createElement('span');
       span.append(keyBoard[row][keys][i]);
       if (i === 0 || i === 1) {
-        document.querySelector('.' + `${keys}` + ' .eng').append(span);
+        document.querySelector(`.${keys} .eng`).append(span);
+        if (i === 1) {
+          span.classList.add('hidden');
+        }
       } else {
-        document.querySelector('.' + `${keys}` + ' .rus').append(span);
+        document.querySelector(`.${keys} .rus`).append(span);
+        if (i === 3) {
+          span.classList.add('hidden');
+        }
       }
     }
   }
 }
 
+const addClickTabHandler = () => {
+  const textArea = document.querySelector('textarea');
+  textArea.value += '  ';
+}
+
+const addClickBackspaceHandler = () => {
+  const textArea = document.querySelector('textarea');
+  textArea.setRangeText('', textArea.selectionStart - 1, textArea.selectionEnd)
+}
+
+const addClickSpaceHandler = () => {
+  const textArea = document.querySelector('textarea');
+  textArea.value += ' ';
+}
+
+const addClickEnterHandler = () => {
+  const textArea = document.querySelector('textarea');
+  textArea.value += '\n'
+}
+
+const addClickSpecialKeysHandler = () => {
+  const textArea = document.querySelector('textarea');
+  textArea.value += '';
+}
+
+const addChangeUppercase = (e) => {
+  const KeyShift = e.target.textContent.substring(0, 5);
+  const keyTarget = e.target.textContent;
+  if (event.code === "ShiftLeft" || event.code === "ShiftRight" || KeyShift === "Shift" || keyTarget === "Shift") {
+    document.querySelectorAll('span span').forEach((elem) => {
+      if (elem.classList.contains('hidden')) {
+        elem.classList.remove('hidden')
+      } else {
+        elem.classList.add('hidden')
+      }
+    })
+  }
+}
+
+const updateValueFromMouse = (e) => {
+  const keyTab = e.target.textContent.substring(0, 3);
+  const KeyBackspace = e.target.textContent.substring(0, 9);
+  const KeySpace = e.target.textContent.substring(0, 5);
+  const KeyEnter = KeyShift = KeySpace;
+  const keyCtrl = e.target.textContent.substring(0, 4);
+  const keyWin = keyAlt = keyTab;
+  const keyTarget = e.target.textContent;
+  // console.log(e.target.textContent);
+  // console.log(e.target.textContent.substring(0, 9));
+  if (e.target.textContent.length > 1) {
+    if (keyTab === "Tab" || keyTarget === "Tab") {
+      addClickTabHandler();
+    } else if (KeyBackspace === "Backspace" || keyTarget === "Backspace") {
+      addClickBackspaceHandler();
+    } else if (KeySpace === "Space" || keyTarget === "Space") {
+      addClickSpaceHandler();
+    } else if (KeyEnter === "Enter" || keyTarget === "Enter") {
+      addClickEnterHandler();
+    } else if (KeyShift === "Shift" || keyCtrl === "Ctrl" || keyWin === "Win" || keyAlt === "Alt" || keyTarget === "Ctrl" || keyTarget === "Win" || keyTarget === "Alt" || keyTarget === "Shift") {
+      addClickSpecialKeysHandler();
+    } else {
+      textArea.value = textArea.value + e.target.querySelector('span span').textContent;
+    }
+  } else {
+    textArea.value = textArea.value + e.target.textContent;
+  }
+}
+
 document.addEventListener('keydown', (event) => {
-  // if (event.code != "Enter" || event.code != "Backspace") {
-  //   event.preventDefault();
-  // }
+  if (event.code === "Tab" || event.altKey) {
+    event.preventDefault();
+  }
+  console.log(event.code);
   document.querySelector(`.${event.code}`).classList.add('background');
-  // updateValueFromKeyBoard(event);
+  addChangeUppercase(event);
 });
 
 document.addEventListener('keyup', (event) => {
   document.querySelector(`.${event.code}`).classList.remove('background');
+  addChangeUppercase(event);
 });
 
 document.querySelector('.keyboard').addEventListener('mousedown', (event) => {
   textArea.focus();
   event.target.classList.add('background');
   updateValueFromMouse(event);
+  addChangeUppercase(event);
 });
 
 document.querySelector('.wrapper').addEventListener('mouseup', (event) => {
   textArea.focus();
   event.target.classList.remove('background');
+  addChangeUppercase(event);
 });
-
-const updateValueFromKeyBoard = (e) => {
-  if (e.code === "Enter") {
-    return textarea.value;
-  }
-  if (e.code != "Backspace")
-  textarea.value = textarea.value + document.querySelector(`.${e.code}`).textContent;
-}
-
-const updateValueFromMouse = (e) => {
-   textarea.value = textarea.value + e.target.textContent;
-}
